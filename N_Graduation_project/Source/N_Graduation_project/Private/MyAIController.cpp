bcad8e7443d37d6af1a5b8163a678409ca722de7
KDA1325 <1325680q@gmail.com>
2025-03-17 00:14:03 +0900

    AIController 생성

---
+++ N_Graduation_project/Source/N_Graduation_project/Private/MyAIController.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyAIController.h"
+
+// Sets default values
+AMyAIController::AMyAIController()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyAIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
