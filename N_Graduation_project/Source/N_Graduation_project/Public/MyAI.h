4a5a21239866d84c669d32b65500aa77bcae9a9f
KDA1325 <1325680q@gmail.com>
2025-03-20 20:36:21 +0900

    BBKEY PlayerLocation 키 추가

--- N_Graduation_project/Source/N_Graduation_project/Public/MyAI.h
+++ N_Graduation_project/Source/N_Graduation_project/Public/MyAI.h
@@ -1,6 +1,7 @@
 
 #pragma once
 
+#define BBKEY_PLAYERLOCATION TEXT("PlayerLocation")
 #define BBKEY_HOMEPOS TEXT("HomePos")
 #define BBKEY_PATROLPOS TEXT("PatrolPos")
 #define BBKEY_TARGET TEXT("Target")
