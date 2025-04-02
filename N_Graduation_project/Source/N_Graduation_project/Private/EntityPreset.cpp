690a3a878d6b1f06f2d92cfd358f5b8c5459b43c
KDA1325 <1325680q@gmail.com>
2025-04-02 17:11:52 +0900

    작업 내용 복구

--- N_Graduation_project/Source/N_Graduation_project/Private/EntityPreset.cpp
+++ N_Graduation_project/Source/N_Graduation_project/Private/EntityPreset.cpp
@@ -1,5 +1,5 @@
-#include "EntityWidget.h"
 #include "EntityPreset.h"
+#include "EntityWidget.h"
 #include "MyAIController.h"
 #include "MyAI.h"
 #include "BehaviorTree/BlackboardComponent.h"
@@ -99,7 +99,7 @@ void AEntityPreset::SetHP(float NewHP)
 	if (CurrentHP <= 0)
 	{
 		UE_LOG(LogTemp, Warning, TEXT("banana Entity Die"));
-		//	Destroy();
+		Destroy();
 	}
 }
 
