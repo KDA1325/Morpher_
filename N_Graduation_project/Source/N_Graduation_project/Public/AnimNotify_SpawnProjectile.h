f0c6859ead53a79f10c125d9984252b90fd63f64
다애 김 <1325680q@gmail.com>
2025-05-05 06:45:49 +0900

    투사체 발사 AnimNotify 클래스 생성

---
+++ N_Graduation_project/Source/N_Graduation_project/Public/AnimNotify_SpawnProjectile.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AnimNotify_SpawnProjectile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class N_GRADUATION_PROJECT_API UAnimNotify_SpawnProjectile : public UAnimNotify
+{
+	GENERATED_BODY()
+	
+};
