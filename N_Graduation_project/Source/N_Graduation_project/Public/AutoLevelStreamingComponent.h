d4e686e300849f3d7ad2cd460472c49d0979a2f3
KDA1325 <1325680q@gmail.com>
2025-05-25 11:20:47 +0900

    레벨스트리밍 구조 수정 중
    연결된 레벨 로드는 모두 정상적으로 작동
    -> 연결되지 않은 레벨 언로드가 안 되는 문제 해결 중

---
+++ N_Graduation_project/Source/N_Graduation_project/Public/AutoLevelStreamingComponent.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "AutoLevelStreamingComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class N_GRADUATION_PROJECT_API UAutoLevelStreamingComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UAutoLevelStreamingComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category = "Level Streaming")
+	TArray<FName> ConnectedLevels;
+
+	UFUNCTION()
+	void OnTriggerOverlap(AActor* OverlappedActor,AActor* OtherActor);
+
+private:
+	// 현재 로드된 레벨 추적용
+	TSet<FName> LoadedLevels; 
+};
