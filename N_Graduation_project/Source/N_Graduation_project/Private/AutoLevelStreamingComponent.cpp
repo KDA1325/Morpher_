5195c0bb8bf8c3d10f2255c92ae38cbf09ea6f6a
KDA1325 <1325680q@gmail.com>
2025-05-27 07:51:25 +0900

    레벨스트리밍 구조 수정 완료

--- N_Graduation_project/Source/N_Graduation_project/Private/AutoLevelStreamingComponent.cpp
+++ N_Graduation_project/Source/N_Graduation_project/Private/AutoLevelStreamingComponent.cpp
@@ -6,6 +6,7 @@
 #include "GameFramework/Actor.h"
 #include "GameFramework/Character.h"
 
+
 // Sets default values for this component's properties
 UAutoLevelStreamingComponent::UAutoLevelStreamingComponent()
 {
@@ -35,81 +36,99 @@ void UAutoLevelStreamingComponent::TickComponent(float DeltaTime, ELevelTick Tic
 {
 }
 
-void UAutoLevelStreamingComponent::OnTriggerOverlap(AActor* OverlappedActor,AActor* OtherActor)
+TArray<FName> UAutoLevelStreamingComponent::GetLoadedStreamingLevelNames(UObject * WorldContextObject)
 {
-	//// 플레이어만 작동하도록 설정
-	//if(!OtherActor->IsA(ACharacter::StaticClass())) return;
+	TArray<FName> LoadedLevelNames;
 
-	////FString LevelRootPath = "/Game/Asset/SyntyAsset/MainMapAsset/PNB_Meadow_Forest/Maps/Forest_Level/";
-
-	//// 현재 레벨에 존재하는 스트리밍 레벨들 가져오기
-	//UWorld* World = GetWorld();
-	//if(!World) return;
-
-	//// 현재 로드된 레벨 이름 저장
-	//TSet<FName> CurrentLoadedLevels;
-
-	//for(ULevelStreaming* StreamingLevel : World->GetStreamingLevels())
-	//{
-	//	if(StreamingLevel && StreamingLevel->IsLevelLoaded())
-	//	{
-	//		FName LevelName = FName(*FPackageName::GetShortFName(StreamingLevel->GetWorldAssetPackageName()).ToString());
-	//		
-	//		CurrentLoadedLevels.Add(LevelName);
-	//	}
-	//}
-
-	//// ConnectedLevels 로드
-	//for(FName LevelName : ConnectedLevels)
-	//{
-	//	//FString FullPath = LevelRootPath + LevelName.ToString();
-	//	//FName FullLevelName(*FullPath);
-
-	//	/*UGameplayStatics::LoadStreamLevel(this,FullLevelName,true,false,FLatentActionInfo());
-	//	LoadedLevels.Add(LevelName);
-
-	//	UE_LOG(LogTemp,Error,TEXT("Load Levels: %s"),*FullLevelName.ToString());*/
-
-	//	if(!CurrentLoadedLevels.Contains(LevelName))
-	//	{
-	//		UGameplayStatics::LoadStreamLevel(this,LevelName,true,false,FLatentActionInfo());
-	//		//LoadedLevels.Add(LevelName);
-
-	//		UE_LOG(LogTemp,Error,TEXT("Load Levels: %s"),*LevelName.ToString());
-	//	}
-	//}
-
-	//for(FName Loaded : CurrentLoadedLevels)
-	//{
-	//	if(!ConnectedLevels.Contains(Loaded))
-	//	{
-	//		//FString FullPath = LevelRootPath + Loaded.ToString();
-	//		//FName FullLevelName(*FullPath);
-
-	//		UGameplayStatics::UnloadStreamLevel(this,Loaded,FLatentActionInfo(),false);
-
-	//		UE_LOG(LogTemp,Error,TEXT("UnLoad Levels: %s"),*Loaded.ToString());
-	//	}
-	//}
-
-	////// 현재 로드되어 있는 레벨 중 ConnectedLevels에 없는 레벨 언로드 
-	////TArray<FName> ToUnload;
-	////for(FName Loaded : LoadedLevels)
-	////{
-	////	FString FullPath = LevelRootPath + Loaded.ToString();
-	////	FName FullLevelName(*FullPath);
-
-	////	if(!ConnectedLevels.Contains(Loaded))
-	////	{
-	////		UGameplayStatics::UnloadStreamLevel(this,FullLevelName,FLatentActionInfo(),false);
-	////		ToUnload.Add(Loaded);
-
-	////		UE_LOG(LogTemp,Error,TEXT("UnLoad Levels: %s"),*FullLevelName.ToString());
-	////	}
-	////}
-
-	///*for(FName LevelName : ToUnload)
-	//{
-	//	LoadedLevels.Remove(LevelName);
-	//}*/
+	if(UWorld* World = GEngine->GetWorldFromContextObjectChecked(WorldContextObject))
+	{
+		for(ULevelStreaming* StreamingLevel : World->GetStreamingLevels())
+		{
+			if(StreamingLevel && StreamingLevel->IsLevelLoaded())
+			{
+				FString PackageName = StreamingLevel->GetWorldAssetPackageName();
+				LoadedLevelNames.Add(FName(*FPackageName::GetShortFName(PackageName).ToString()));
+			}
+		}
+	}
+	return LoadedLevelNames;
 }
+//
+//void UAutoLevelStreamingComponent::OnTriggerOverlap(AActor* OverlappedActor,AActor* OtherActor)
+//{
+//	//// 플레이어만 작동하도록 설정
+//	//if(!OtherActor->IsA(ACharacter::StaticClass())) return;
+//
+//	////FString LevelRootPath = "/Game/Asset/SyntyAsset/MainMapAsset/PNB_Meadow_Forest/Maps/Forest_Level/";
+//
+//	//// 현재 레벨에 존재하는 스트리밍 레벨들 가져오기
+//	//UWorld* World = GetWorld();
+//	//if(!World) return;
+//
+//	//// 현재 로드된 레벨 이름 저장
+//	//TSet<FName> CurrentLoadedLevels;
+//
+//	//for(ULevelStreaming* StreamingLevel : World->GetStreamingLevels())
+//	//{
+//	//	if(StreamingLevel && StreamingLevel->IsLevelLoaded())
+//	//	{
+//	//		FName LevelName = FName(*FPackageName::GetShortFName(StreamingLevel->GetWorldAssetPackageName()).ToString());
+//	//		
+//	//		CurrentLoadedLevels.Add(LevelName);
+//	//	}
+//	//}
+//
+//	//// ConnectedLevels 로드
+//	//for(FName LevelName : ConnectedLevels)
+//	//{
+//	//	//FString FullPath = LevelRootPath + LevelName.ToString();
+//	//	//FName FullLevelName(*FullPath);
+//
+//	//	/*UGameplayStatics::LoadStreamLevel(this,FullLevelName,true,false,FLatentActionInfo());
+//	//	LoadedLevels.Add(LevelName);
+//
+//	//	UE_LOG(LogTemp,Error,TEXT("Load Levels: %s"),*FullLevelName.ToString());*/
+//
+//	//	if(!CurrentLoadedLevels.Contains(LevelName))
+//	//	{
+//	//		UGameplayStatics::LoadStreamLevel(this,LevelName,true,false,FLatentActionInfo());
+//	//		//LoadedLevels.Add(LevelName);
+//
+//	//		UE_LOG(LogTemp,Error,TEXT("Load Levels: %s"),*LevelName.ToString());
+//	//	}
+//	//}
+//
+//	//for(FName Loaded : CurrentLoadedLevels)
+//	//{
+//	//	if(!ConnectedLevels.Contains(Loaded))
+//	//	{
+//	//		//FString FullPath = LevelRootPath + Loaded.ToString();
+//	//		//FName FullLevelName(*FullPath);
+//
+//	//		UGameplayStatics::UnloadStreamLevel(this,Loaded,FLatentActionInfo(),false);
+//
+//	//		UE_LOG(LogTemp,Error,TEXT("UnLoad Levels: %s"),*Loaded.ToString());
+//	//	}
+//	//}
+//
+//	////// 현재 로드되어 있는 레벨 중 ConnectedLevels에 없는 레벨 언로드 
+//	////TArray<FName> ToUnload;
+//	////for(FName Loaded : LoadedLevels)
+//	////{
+//	////	FString FullPath = LevelRootPath + Loaded.ToString();
+//	////	FName FullLevelName(*FullPath);
+//
+//	////	if(!ConnectedLevels.Contains(Loaded))
+//	////	{
+//	////		UGameplayStatics::UnloadStreamLevel(this,FullLevelName,FLatentActionInfo(),false);
+//	////		ToUnload.Add(Loaded);
+//
+//	////		UE_LOG(LogTemp,Error,TEXT("UnLoad Levels: %s"),*FullLevelName.ToString());
+//	////	}
+//	////}
+//
+//	///*for(FName LevelName : ToUnload)
+//	//{
+//	//	LoadedLevels.Remove(LevelName);
+//	//}*/
+//}
--- N_Graduation_project/Source/N_Graduation_project/Public/AutoLevelStreamingComponent.h
+++ N_Graduation_project/Source/N_Graduation_project/Public/AutoLevelStreamingComponent.h
@@ -24,13 +24,16 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+	UFUNCTION(BlueprintCallable,Category="Level Streaming")
+		static TArray<FName> GetLoadedStreamingLevelNames(UObject* WorldContextObject);
+
 	//UPROPERTY(EditAnywhere,BlueprintReadWrite,Category = "Level Streaming")
 	//TArray<FName> ConnectedLevels;
 
-	UFUNCTION()
-	void OnTriggerOverlap(AActor* OverlappedActor,AActor* OtherActor);
+	/*UFUNCTION()
+	void OnTriggerOverlap(AActor* OverlappedActor,AActor* OtherActor);*/
 
 private:
 	// 현재 로드된 레벨 추적용
-	TSet<FName> LoadedLevels; 
+	//TSet<FName> LoadedLevels; 
 };
