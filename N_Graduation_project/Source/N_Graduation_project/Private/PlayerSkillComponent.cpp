55960dba40192601358b25314c471f733428de14
audwn <ajdwn07@gmail.com>
2025-05-30 23:46:42 +0900

    해골 전사 노말 스킬만 방어하기

--- N_Graduation_project/Source/N_Graduation_project/Private/PlayerSkillComponent.cpp
+++ N_Graduation_project/Source/N_Graduation_project/Private/PlayerSkillComponent.cpp
@@ -1100,7 +1100,7 @@ void UPlayerSkillComponent::SkillEffect(const FString& SkillNameID)
 					}
 					//GEngine->AddOnScreenDebugMessage(-1,3.0f,FColor::Red,TEXT("Damage 실행됨"));
 				} else {
-					UGameplayStatics::ApplyDamage(TargetActor,700,MyChar->GetController(),MyChar,nullptr);
+					UGameplayStatics::ApplyDamage(TargetActor,200,MyChar->GetController(),MyChar,nullptr);
 					UE_LOG(LogTemp,Warning,TEXT("ㅊㅊㅊ %s Damage: %f"),*TargetActor->GetName(),DamageAmount);
 					if(TargetActor->ActorHasTag(FName("Barrel")))
 					{
---
+++ N_Graduation_project/Source/N_Graduation_project/Public/NormalAttackDamageType.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/DamageType.h"
+#include "NormalAttackDamageType.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class N_GRADUATION_PROJECT_API UNormalAttackDamageType : public UDamageType
+{
+	GENERATED_BODY()
+	
+};
