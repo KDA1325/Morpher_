5a046c7e59ff5dbe2826c24c85ecc33d3a4c9c78
audwn <ajdwn07@gmail.com>
2025-06-04 02:31:31 +0900

    토템 소리 줄임

--- N_Graduation_project/Source/N_Graduation_project/N_Graduation_projectCharacter.cpp
+++ N_Graduation_project/Source/N_Graduation_project/N_Graduation_projectCharacter.cpp
@@ -192,6 +192,7 @@ void AN_Graduation_projectCharacter::BeginPlay()
 	// Call the base class    
 	Super::BeginPlay();
 	m_pMeshCom = GetMesh();
+	InitFootstepSounds();
 	PlayFootstepSound();
 	FOnTimelineFloat DashCallback;
 	SpawnHitBoxAtSocket("AttachHitBox");
