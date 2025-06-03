d1773e6c6ff579bda1c7b080d2faa290cf857ce2
KDA1325 <1325680q@gmail.com>
2025-06-04 07:42:13 +0900

    몬스터 히트박스 안 보이게 처리

--- N_Graduation_project/Source/N_Graduation_project/Private/EntityPreset.cpp
+++ N_Graduation_project/Source/N_Graduation_project/Private/EntityPreset.cpp
@@ -1047,7 +1047,7 @@ void AEntityPreset::ShowNormalHitBox()
 
 	// HitBox 활성화 
 	NormalSkillHitBox->SetHiddenInGame(false);
-	NormalSkillHitBox->SetVisibility(true);
+	//NormalSkillHitBox->SetVisibility(true);
 	NormalSkillHitBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);  // 충돌 켜기, 물리는 무시하지만 쿼리만 처리
 	UE_LOG(LogTemp,Warning,TEXT("Show HitBox"));
 
@@ -1071,7 +1071,7 @@ void AEntityPreset::ShowSpecialHitBox()
 
 	// HitBox 활성화 
 	SpecialSkillHitBox->SetHiddenInGame(false);
-	SpecialSkillHitBox->SetVisibility(true);
+	//SpecialSkillHitBox->SetVisibility(true);
 	SpecialSkillHitBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);  // 충돌 켜기
 	UE_LOG(LogTemp,Warning,TEXT("Show HitBox"));
 
@@ -1086,7 +1086,7 @@ void AEntityPreset::ShowSpecialSphereHitBox()
 
 	// HitBox 활성화 
 	SpecialSkillSphereHitBox->SetHiddenInGame(false);
-	SpecialSkillSphereHitBox->SetVisibility(true);
+	//SpecialSkillSphereHitBox->SetVisibility(true);
 	SpecialSkillSphereHitBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);  // 충돌 켜기
 	UE_LOG(LogTemp,Warning,TEXT("Show Sphere HitBox"));
 
@@ -1099,7 +1099,7 @@ void AEntityPreset::HideSpecialHitBox()
 {
 	// HitBox 비활성화 
 	SpecialSkillHitBox->SetHiddenInGame(true);
-	SpecialSkillHitBox->SetVisibility(false);
+	//SpecialSkillHitBox->SetVisibility(false);
 	SpecialSkillHitBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);  // 충돌 끄기
 	UE_LOG(LogTemp,Warning,TEXT("Hide HitBox"));
 }
